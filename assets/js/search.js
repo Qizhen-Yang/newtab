34feeb771bdcf8d376041b202d3fb9fa668f2111
Qizhen-Yang <qizhen_yang@163.com>
2021-10-07 11:21:36 +0800

    added sticky notes weeeeeeeeeeeeeeeee

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,30 +1,12 @@
-function baidu()
+function search()
 {
-	var baidu=document.getElementById("baidu");
-	window.open("https://www.baidu.com/s?wd="+baidu.value);
-	location.reload();
-}
-function bing()
-{
-	var bing=document.getElementById("bing");
-	window.open("https://www.bing.com/search?q="+bing.value);
-	location.reload();
-}
-function google()
-{
-	var google=document.getElementById("google");
-	window.open("https://www.google.com/search?q="+google.value);
-	location.reload();
-}
-function googlem()
-{
-	var googlem=document.getElementById("googlem");
-	window.open("https://goo.gle.workers.dev/search?q="+googlem.value);
-	location.reload();
-}
-function zhihu()
-{
-	var zhihu=document.getElementById("zhihu");
-	window.open("https://www.zhihu.com/search?q="+zhihu.value);
-	location.reload();
+	var n = document.getElementById("engines").selectedIndex;
+	var url = [
+			"https://www.baidu.com/s?wd=",
+			"https://www.bing.com/search?q=",
+			"https://goo.gle.workers.dev/search?q=",
+			"https://www.zhihu.com/search?q=",
+			"https://www.google.com/search?q="];
+	window.open(url[n] + document.getElementById("searchbox").value);
+	document.getElementById("searchbox").value = "";
 }
---
+++ assets/js/sticky.js
@@ -0,0 +1,10 @@
+window.onload = function ()
+{
+	document.getElementById("sticky").value = localStorage.getItem("newtabSticky");
+}
+
+function save()
+{
+	localStorage.setItem("newtabSticky", document.getElementById("sticky").value);
+	document.getElementById("savelog").innerText = new Date().toLocaleString() + "\t保存到了 localStorage";
+}
