37dbd7995ef4c89ddfacbbc33793e225ec6536c9
Qizhen-Yang <qizhen_yang@163.com>
2021-10-09 22:37:58 +0800

    revised markdown

--- assets/js/sticky.js
+++ assets/js/sticky.js
@@ -1,18 +1,18 @@
-const EMPTY = "便笺内容为空"
+const EMPTY = "无内容"
 const PLACEHOLDER  = "随便记点什么 ..."
-
-read();
-preview();
 
 function read()
 {
 	var content = localStorage.getItem("newtabSticky");
-	document.getElementById("sticky").value = content;
-	document.getElementById("md").innerHTML = marked(content);
-	if (content == 0)
+	if (content == 0 || content == null)
 	{
 		document.getElementById("savelog").innerHTML = EMPTY;
 		document.getElementById("md").innerHTML = PLACEHOLDER;
+	}
+	else
+	{
+		document.getElementById("sticky").value = content;
+		document.getElementById("md").innerHTML = marked(content);
 	}
 }
 
@@ -20,12 +20,15 @@ function save()
 {
 	var content = document.getElementById("sticky").value;
 	localStorage.setItem("newtabSticky", content);
-	document.getElementById("md").innerHTML = marked(content);
-	document.getElementById("savelog").innerHTML = new Date().toLocaleString() + " 保存到了 localStorage";
-	if (content == 0)
+	if (content == 0 || content == null)
 	{
 		document.getElementById("savelog").innerHTML = EMPTY;
 		document.getElementById("md").innerHTML = PLACEHOLDER;
+	}
+	else
+	{
+		document.getElementById("md").innerHTML = marked(content);
+		document.getElementById("savelog").innerHTML = "已保存";
 	}
 }
 
@@ -40,13 +43,21 @@ function printnote()
 
 function edit()
 {
+	document.getElementById("sticky").style.height = "20rem";
+	document.getElementById("sticky").style.padding = "0.75rem 1rem";
+	document.getElementById("sticky").style.border = "solid 1px #ffffff";
 	document.getElementById("md").style.display = "none";
-	document.getElementById("sticky").style.display = "block";
 	document.getElementById("sticky").focus();
 }
 
 function preview()
 {
-	document.getElementById("sticky").style.display = "none";
+	document.getElementById("sticky").style.height = "0";
+	document.getElementById("sticky").style.padding = "0";
+	document.getElementById("sticky").style.border = "transparent";
 	document.getElementById("md").style.display = "block";
+	document.getElementById("sticky").focus();
 }
+
+read();
+preview();
