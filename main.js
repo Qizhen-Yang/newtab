acd7716d3cf6070e47dc8eb64aa754764459cf65
Qizhen Yang <qizhen_yang@163.com>
2022-08-25 08:21:28 +0800

    调整picaday

--- main.js
+++ main.js
@@ -182,7 +182,7 @@ window.onload = function () {
 	updateTime();
 	$.ajax({
 		type: 'GET',
-		url: 'https://picaday.vercel.app/Picaday/picaday.json',
+		url: 'https://picaday.vercel.app/picaday.json',
 		dataType: 'json',
 		success(data) {
 			msg('图库请求成功', 'success')
