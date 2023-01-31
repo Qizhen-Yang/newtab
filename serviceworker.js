43a7c9873774bd3a63fdda78fedd0161aadf19b2
Qizhen Yang <qizhen_yang@163.com>
2023-01-31 10:00:27 +0800

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -23,24 +23,17 @@ this.addEventListener('install', function (event) {
 
 /* 注册fetch事件，拦截全站的请求 */
 this.addEventListener('fetch', function (event) {
-	console.log(`Handling fetch event for ${event.request.url}`);
-
 	event.respondWith(
 		caches.match(event.request).then((response) => {
 			if (response) {
-				console.log("Found response in cache:", response);
 				return response;
 			}
-			console.log("No response found in cache. About to fetch from network…");
 
 			return fetch(event.request)
 				.then((response) => {
-					console.log("Response from network is:", response);
-
 					return response;
 				})
 				.catch((error) => {
-					console.error(`Fetching failed: ${error}`);
 					throw error;
 				});
 		})
